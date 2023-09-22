fcc5c26fe8effe21237971a87a92611a65ab4a46
unknown <rinoally@gmail.com>
2023-09-22 15:44:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <div>
+      <p>12123</p>
+    </div>
+  )
+}
+
+export default App
