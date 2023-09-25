94b8ba09575d4d714bdcc157dbc032584de8de60
Victor Golovin <rinoally@gmail.com>
2023-09-25 16:16:30 +0300

    add code for inputs

--- src/FormSection/FormSection.jsx
+++ src/FormSection/FormSection.jsx
@@ -1,74 +1,113 @@
 import React from "react";
 import { useState } from "react";
-import './FormSection.css'
-import FormTitle from "../FormTitle/FormTitle"
-import FormText from "../FormText/FormText"
-
+import "./FormSection.css";
+import FormTitle from "../FormTitle/FormTitle";
+import FormText from "../FormText/FormText";
 
 const FormSection = () => {
+
+  const [formRegistering, setFormRegistering] = useState({
+    name: 'name',
+    surname: 'surname',
+    number: 'number',
+    email: 'email',
+
+  });
+
+  const handleNameInputChange = (event) => {
+    setFormRegistering ({
+      ...formRegistering,
+      name: event.target.value,
+      surname: event.target.value,
+      number: event.target.value,
+      email: event.target.value,
+      password: event.target.value,
+      repeatpassword: event.target.value
+    })
+    console.log(event.target.value)
+  }
+
+  const handleFormBtn = () => {
+    console.log('press')
+  }
+
   return (
     <div className="container">
       <div className="form-section-inner">
         <FormTitle />
         <FormText />
-      <div className="form-inputs">
-      <input 
-        className="form-input"
-        type="text" 
-        name="name" 
-        placeholder="Имя" />
+        <div className="form-inputs">
+          <input
+            className="form-input"
+            type="text"
+            name="name"
+            placeholder="Имя"
+            onChange={handleNameInputChange}
+          />
 
-        <input 
-        className="form-input"
-        type="text" 
-        name="surname" 
-        placeholder="Фамилия" />
+          <input
+            className="form-input"
+            type="text"
+            surname="surname"
+            placeholder="Фамилия"
+            onChange={handleNameInputChange}
+          />
 
-        <input 
-        className="form-input"
-        type="text" 
-        name="number" 
-        placeholder="Номер телефона" />
+          <input
+            className="form-input"
+            type="text"
+            number="number"
+            placeholder="Номер телефона"
+            onChange={handleNameInputChange}
+          />
 
-        <input 
-        className="form-input"
-        type="text" 
-        name="email" 
-        placeholder="Email" />
+          <input
+            className="form-input"
+            type="text"
+            email="email"
+            placeholder="Email"
+            onChange={handleNameInputChange}
+          />
 
-        <input 
-        className="form-input"
-        type="text" 
-        name="pass" 
-        placeholder="Пароль" />
+          <input
+            className="form-input"
+            type="text"
+            password="password"
+            placeholder="Пароль"
+            onChange={handleNameInputChange}
+          />
 
-        <input 
-        className="form-input"
-        type="text" 
-        name="repeat-pass" 
-        placeholder="Повторите пароль" />
+          <input
+            className="form-input"
+            type="text"
+            repeatpassword="repeatpassword"
+            placeholder="Повторите пароль"
+            onChange={handleNameInputChange}
+          />
 
-        <div className="form-checkbox-inner">
-          <label className="custom-checkbox" htmlFor="form-checkbox">
-            <input 
-          className="form-checkbox"
-          type="checkbox" 
-          name="checkbox" 
-          id="form-checkbox" />
-          Подтверждаю пароль
-          </label>
+          <div className="form-checkbox-inner">
+            <label className="custom-checkbox" htmlFor="form-checkbox">
+              <input
+                className="form-checkbox"
+                type="checkbox"
+                name="checkbox"
+                id="form-checkbox"
+              />
+              Подтверждаю пароль
+            </label>
+          </div>
         </div>
-      </div>
-        
 
-      <button className="form-button">Продолжить</button>
+        <button 
+        className="form-button"
+        onClick={handleFormBtn}>Продолжить</button>
 
-      <div className="form-bottom-text-inner">
-        <p className="form-bottom-text">Уже есть аккаунт ?</p>
-        <a className="form-bottom-link" href="/">Войти</a>
-      </div>
-      
-
+        <div className="form-bottom-text-inner">
+          <p className="form-bottom-text">Уже есть аккаунт ?</p>
+          <a className="form-bottom-link" href="/">
+            Войти
+          </a>
+        </div>
       </div>
     </div>
   );
